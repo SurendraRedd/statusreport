e95a9013d3fa80bc35202a27df97d66258717948
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2024-03-30 11:55:16 +0000

    initial development

--- app.py
+++ app.py
@@ -3,12 +3,6 @@ This application contains the code related to the
 Azure SaaS Release - Round 2 Status Details dashboard.
 """
 
-__author__ = 'Surendra Reddy'
-__version__ = '1.0'
-__maintainer__ = 'Surendra Reddy'
-__email__ = 'tbd'
-__status__ = 'Prototype'
-
 import streamlit as st  # noqa: E402
 import datetime  # noqa: E402
 import streamlit_shadcn_ui as ui  # noqa: E402
@@ -17,16 +11,7 @@ from streamlit_star_rating import st_star_rating  # noqa: E402
 from streamlit_option_menu import option_menu
 from streamlit_extras.metric_cards import style_metric_cards
 
-
-print('# ' + '=' * 78)
-print('Author: ' + __author__)
-print('Version: ' + __version__)
-print('Maintainer: ' + __maintainer__)
-print('Email: ' + __email__)
-print('Status: ' + __status__)
-print('# ' + '=' * 78)
-
-st.set_page_config(layout="wide", page_title="Azure SaaS Release - Round 2 Status", page_icon="🎄")
+st.set_page_config(layout="wide", page_title="Azure SaaS Release - Round 2 Status", page_icon=":writing_hand:")
 
 def load_gif(workflow_name):
     if workflow_name == "System Preparation & Initial Deployment":
@@ -35,32 +20,33 @@ def load_gif(workflow_name):
         data_url = base64.b64encode(contents).decode("utf-8")
         file_.close()
 
-        st.markdown(
-            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-            unsafe_allow_html=True,
-        )
+        with st.expander("See workflow below"):
+            st.markdown(
+                f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+                unsafe_allow_html=True,
+            )
 
     if workflow_name == "Update Configuration":
         file_ = open("./update_config.gif", "rb")
         contents = file_.read()
         data_url = base64.b64encode(contents).decode("utf-8")
         file_.close()
-
-        st.markdown(
-            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-            unsafe_allow_html=True,
-        )
+        with st.expander("See workflow below"):
+            st.markdown(
+                f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+                unsafe_allow_html=True,
+            )
     
     if workflow_name == "Uninstallation":
         file_ = open("./uninstall.gif", "rb")
         contents = file_.read()
         data_url = base64.b64encode(contents).decode("utf-8")
         file_.close()
-
-        st.markdown(
-            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-            unsafe_allow_html=True,
-        )
+        with st.expander("See workflow below"):
+            st.markdown(
+                f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+                unsafe_allow_html=True,
+            )
 
 # Define function to create stages for each workflow
 def create_workflow_stages(workflow_name):
@@ -74,25 +60,43 @@ def create_workflow_stages(workflow_name):
         with st.expander("Stages", expanded=True):
             col1, col2, col3, col4, col5, col6 = st.columns(6)
             with col1:
-                switch_value1 = ui.switch(default_checked=False, label="Initial SEQ", key="switch1")
-                st.write("Completed:", switch_value1)
+                switch_value1 = ui.switch(default_checked=False, label="InitialSEQ(Yes/No)", key="switch1")
+                if switch_value1:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col2:
-                switch_value2 = ui.switch(default_checked=False, label="Inst Plan & Report", key="switch2")
-                st.write("Completed:", switch_value2)
+                switch_value2 = ui.switch(default_checked=False, label="InstPlan&Report(Yes/No)", key="switch2")
+                if switch_value2:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col3:
-                switch_value3 = ui.switch(default_checked=False, label="Deploy Plan & Report", key="switch3")
-                st.write("Completed:", switch_value3)
+                switch_value3 = ui.switch(default_checked=False, label="DepPlan&Report(Yes/No)", key="switch3")
+                if switch_value3:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col4:
-                switch_value4 = ui.switch(default_checked=False, label="Deployment SEQ", key="switch4")
-                st.write("Completed:", switch_value4)
+                switch_value4 = ui.switch(default_checked=False, label="DeploymntSEQ(Yes/No)", key="switch4")
+                if switch_value4:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col5:
-                switch_value5 = ui.switch(default_checked=False, label="Inst Qualification", key="switch5")
-                st.write("Completed:", switch_value5)
+                switch_value5 = ui.switch(default_checked=False, label="InstQuali(Yes/No)", key="switch5")
+                if switch_value5:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col6:
-                switch_value6 = ui.switch(default_checked=False, label="Servat Execution", key="switch6")
-                st.write("Completed:", switch_value6)
+                switch_value6 = ui.switch(default_checked=False, label="ServatExecution(Yes/No)", key="switch6")
+                if switch_value6:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
 
-            txt = st.text_area("Notes", "Enter any observations")
+            txt = st.text_area("Notes", "")
             st.write("---")
 
             if switch_value1 and switch_value2 and switch_value3 and switch_value4 and switch_value5 and switch_value6:
@@ -107,22 +111,34 @@ def create_workflow_stages(workflow_name):
         st.write("\n")
         st.write("\n")
 
-        with st.expander("Status", expanded=True):
+        with st.expander("Stages", expanded=True):
             col1, col2, col3, col4 = st.columns(4)
             with col1:
-                uc_switch_value1 = ui.switch(default_checked=False, label="Deployment SEQ", key="ucswitch1")
-                st.write("Completed:", uc_switch_value1)
+                uc_switch_value1 = ui.switch(default_checked=False, label="Deployment SEQ (Yes/No)", key="ucswitch1")
+                if uc_switch_value1:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col2:
-                uc_switch_value2 = ui.switch(default_checked=False, label="Installation Plan & Report", key="ucswitch2")
-                st.write("Completed:", uc_switch_value2)
+                uc_switch_value2 = ui.switch(default_checked=False, label="Installation Plan & Report (Yes/No)", key="ucswitch2")
+                if uc_switch_value2:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col3:
-                uc_switch_value3 = ui.switch(default_checked=False, label="Update Config SEQ", key="ucswitch3")
-                st.write("Completed:", uc_switch_value3)
+                uc_switch_value3 = ui.switch(default_checked=False, label="Update Config SEQ (Yes/No)", key="ucswitch3")
+                if uc_switch_value3:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col4:
-                uc_switch_value4 = ui.switch(default_checked=False, label="Servat Execution", key="ucswitch4")
-                st.write("Completed:", uc_switch_value4)
+                uc_switch_value4 = ui.switch(default_checked=False, label="Servat Execution (Yes/No)", key="ucswitch4")
+                if uc_switch_value4:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
 
-            txt = st.text_area("Notes", "Enter any observations")
+            txt = st.text_area("Notes", "")
             st.write("---")
 
             if uc_switch_value1 and uc_switch_value2 and uc_switch_value3 and uc_switch_value4:
@@ -137,22 +153,34 @@ def create_workflow_stages(workflow_name):
         st.write("\n")
         st.write("\n")
 
-        with st.expander("Status", expanded=True):
+        with st.expander("Stages", expanded=True):
             col1, col2, col3, col4 = st.columns(4)
             with col1:
-                un_switch_value1 = ui.switch(default_checked=False, label="Deployment SEQ", key="unswitch1")
-                st.write("Completed:", un_switch_value1)
+                un_switch_value1 = ui.switch(default_checked=False, label="Deployment SEQ (Yes/No)", key="unswitch1")
+                if un_switch_value1:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col2:
-                un_switch_value2 = ui.switch(default_checked=False, label="Installation Plan & Report", key="unswitch2")
-                st.write("Completed:", un_switch_value2)
+                un_switch_value2 = ui.switch(default_checked=False, label="Installation Plan & Report (Yes/No)", key="unswitch2")
+                if un_switch_value2:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col3:
-                un_switch_value3 = ui.switch(default_checked=False, label="Update Config SEQ", key="unswitch3")
-                st.write("Completed:", un_switch_value3)
+                un_switch_value3 = ui.switch(default_checked=False, label="Update Config SEQ (Yes/No)", key="unswitch3")
+                if un_switch_value3:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
             with col4:
-                un_switch_value4 = ui.switch(default_checked=False, label="Servat Execution", key="unswitch4")
-                st.write("Completed:", un_switch_value4)
+                un_switch_value4 = ui.switch(default_checked=False, label="Servat Execution (Yes/No)", key="unswitch4")
+                if un_switch_value4:
+                    st.metric(label=":point_down: Completed", value="Yes", delta="")
+                else:
+                    st.metric(label=":point_down: Completed", value="No", delta="")
 
-            txt = st.text_area("Notes", "Enter any observations")
+            txt = st.text_area("Notes", "")
             st.write("---")
 
             if un_switch_value1 and un_switch_value2 and un_switch_value3 and un_switch_value4:
@@ -189,10 +217,8 @@ def home():
     """
     st.markdown(hide_footer_style, unsafe_allow_html=True)
 
-    st.title(":point_down: Azure SaaS Release - Round 2 Status Details")
+    st.title(":point_down: Azure SaaS Release - :blue[Round 2 Status Details]")
     st.markdown("---")
-    st.write("\n")
-    st.write("\n")
     st.write("\n")
 
     st.subheader("Phases")
@@ -207,40 +233,39 @@ def home():
         with st.expander("Details", expanded=True):
             home_switch_value1 = ui.switch(default_checked=False, label="System Preparation (Yes/No)", key="homeswitch1")
             if home_switch_value1:
-                st.metric(label=":point_down: Status", value="Yes", delta="")
+                st.metric(label=":point_down: Completed", value="Yes", delta="")
             else:
-                st.metric(label=":point_down: Status", value="No", delta="")
+                st.metric(label=":point_down: Completed", value="No", delta="")
 
     with col2:
         st.subheader("Initial Deployment")
         with st.expander("Details", expanded=True):
             home_switch_value2 = ui.switch(default_checked=False, label="Initial Deployment (Yes/No)", key="homeswitch2")
             if home_switch_value2:
-                st.metric(label=":point_down: Status", value="Yes", delta="")
+                st.metric(label=":point_down: Completed", value="Yes", delta="")
             else:
-                st.metric(label=":point_down: Status", value="No", delta="")
+                st.metric(label=":point_down: Completed", value="No", delta="")
 
     with col3:
         st.subheader("Update Configuration")
         with st.expander("Details", expanded=True):
             home_switch_value3 = ui.switch(default_checked=False, label="Update Configuration (Yes/No)", key="homeswitch3")
             if home_switch_value3:
-                st.metric(label=":point_down: Status", value="Yes", delta="")
+                st.metric(label=":point_down: Completed", value="Yes", delta="")
             else:
-                st.metric(label=":point_down: Status", value="No", delta="")           
+                st.metric(label=":point_down: Completed", value="No", delta="")           
 
     with col4:
         st.subheader("Uninstallation")
         with st.expander("Details", expanded=True):
             home_switch_value4 = ui.switch(default_checked=False, label="Uninstallation (Yes/No)", key="homeswitch4")
             if home_switch_value4:
-                st.metric(label=":point_down: Status", value="Yes", delta="")
+                st.metric(label=":point_down: Completed", value="Yes", delta="")
             else:
-                st.metric(label=":point_down: Status", value="No", delta="")
+                st.metric(label=":point_down: Completed", value="No", delta="")
 
     if home_switch_value1 and home_switch_value2 and home_switch_value3 and home_switch_value4:
         st.success('Overall Status: :point_right: Completed')
-        st.balloons()
     elif not home_switch_value1 and not home_switch_value2 and not home_switch_value3 and not home_switch_value4:
         st.warning('Overall Status: :point_right: Not Started')
     else:
@@ -269,7 +294,8 @@ def workflow1():
         </div>
         </body>
     """
-    st.markdown(html_temp, unsafe_allow_html=True)
+    st.title("Workflow: System Preparation & Initial Deployment")
+    #st.markdown(html_temp, unsafe_allow_html=True)
 
     hide_footer_style = """
     <style>
@@ -296,7 +322,8 @@ def workflow2():
         </div>
         </body>
     """
-    st.markdown(html_temp, unsafe_allow_html=True)
+    #st.markdown(html_temp, unsafe_allow_html=True)
+    st.title("Workflow: Update Configuration")
 
     hide_footer_style = """
     <style>
@@ -323,8 +350,8 @@ def workflow3():
         </div>
         </body>
     """
-    st.markdown(html_temp, unsafe_allow_html=True)
-
+    #st.markdown(html_temp, unsafe_allow_html=True)
+    st.title("Workflow: Uninstallation")
     hide_footer_style = """
     <style>
     .reportview-container .main footer {visibility: hidden;}    
@@ -336,6 +363,7 @@ def workflow3():
 # Create navigation
 #navigation = st.sidebar.radio("Navigation", ["Home", "System Prep + Initial Deployment", "Update Configuration", "Uninstallation"])
 with st.sidebar:
+    st.image('./Logo.png', caption='')
     selected2 = option_menu(None, ["Home", "System Prep + Initial Deployment", "Update Configuration", 'Uninstallation'], 
         icons=['house', 'cloud-upload', "list-task", 'gear'], 
         menu_icon="cast", default_index=0)
