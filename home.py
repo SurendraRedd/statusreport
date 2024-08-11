9d3bf1fa988d5b8c5e9ac6e088bcbcf6d36c3569
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2024-08-11 06:52:26 +0000

    updates.

--- home.py
+++ home.py
@@ -2,7 +2,12 @@ import streamlit as st
 import extra_streamlit_components as stx
 
 
-st.subheader(":blue[Servat Formal Release Execution Status Tool]")
+st.subheader(":blue[Formal Release Execution Status]")
 st.markdown("---")
-
 st.write("\n")
+
+content = """
+Page provides the details of the servat executions during the formal release of the components, provisioners and saas
+"""
+
+st.markdown(content)
