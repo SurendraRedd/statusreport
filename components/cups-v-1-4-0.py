a52e295787fe44aa00a7a1a882a28f6ad1eac5ff
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2024-10-02 18:56:02 +0000

    updates

--- components/cups-v-1-4-0.py
+++ components/cups-v-1-4-0.py
@@ -227,7 +227,7 @@ with tab3:
             else:
                 st.metric(label=":checkered_flag: Completed", value="No", delta="")
 
-    txt2 = st.text_area(":memo: Observations", value=st.session_state.observations)
+    txt2 = st.text_area(":memo: Observations", value=st.session_state.observations1)
     st.session_state.observations1 = txt2
 
     if home_switch_value5 and home_switch_value6:
