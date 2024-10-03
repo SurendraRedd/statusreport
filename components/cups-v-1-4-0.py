f40685b273c46fa5d1fff76c6eb1d89b6aa02805
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2024-10-03 16:12:16 +0000

    updates

--- components/cups-v-1-4-0.py
+++ components/cups-v-1-4-0.py
@@ -256,7 +256,7 @@ with tab4:
             to_do([(st.write, ":memo: Installation SEQ Signature Completed?")], "31")
             to_do([(st.write, ":memo: Servat Execution Completed?")], "32")
             home_switch_value7 = st.checkbox(":memo: Installation (Yes/No)", value=st.session_state.home_switches["homeswitch7"], key="homeswitch7")
-            st.session_state.home_switches["homeswitch7"] = home_switch_value5
+            st.session_state.home_switches["homeswitch7"] = home_switch_value7
             if home_switch_value7:
                 st.metric(label=":checkered_flag: Completed", value="Yes", delta="")
             else:
@@ -269,7 +269,7 @@ with tab4:
             to_do([(st.write, ":memo: Uninstallation SEQ Signature Completed?")], "35")
             to_do([(st.write, ":memo: Servat Execution Completed?")], "36")
             home_switch_value8 = st.checkbox(":memo: Uninstallation (Yes/No)", value=st.session_state.home_switches["homeswitch8"], key="homeswitch8")
-            st.session_state.home_switches["homeswitch8"] = home_switch_value4
+            st.session_state.home_switches["homeswitch8"] = home_switch_value8
             if home_switch_value8:
                 st.metric(label=":checkered_flag: Completed", value="Yes", delta="")
             else:
